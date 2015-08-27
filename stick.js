8076507e99acb1507bd77b01ab4295915217af32
golovewkin <golovininfo@gmail.com>
2015-08-27 08:50:46 +0300

    release

--- stick.js
+++ stick.js
@@ -1,31 +1,38 @@
 /**
  * Created by Den on 25.08.2015.
  */
+(function ($) {
+    $.fn.stickMenu = function () {
 
-var menu = $('#stick-menu');
-menu.each(function() {
-    if (!($(this).find('li').hasClass("active"))) {
+        var menu = $(this);
+           //ставим активным первый li
+            if (!(menu.find('li').hasClass("active"))) {
 
-        $(this).find('li').first().addClass('active');
-        //return false;
-    }
-});
-var line =  $('#line');
-var active = menu.find('.active');
+                this.find('li').first().addClass('active');
 
-var default_pos = active.offset().left - menu.offset().left;
-var default_width = active.outerWidth();
-line.css({left: default_pos, width: default_width});
-menu.find('li').hover(function() {
-    var self = $(this);
-    var diff = self.offset().left - menu.offset().left;
-    line.stop().animate({
-        width: self.outerWidth(),
-        left: diff
-    }, 500);
-}, function() {
-    line.stop().animate({
-        width: default_width,
-        left: default_pos
-    },500);
-});
+            }
+
+        var line = $('#line');
+        var active = menu.find('.active');
+
+        var default_pos = active.offset().left - menu.offset().left;
+        var default_width = active.outerWidth();
+        line.css({left: default_pos, width: default_width});
+        menu.find('li').hover(function () {
+
+            var self = $(this);
+            var diff = self.offset().left - menu.offset().left;
+            line.stop().animate({
+                width: self.outerWidth(),
+                left: diff
+            }, 500);
+
+        }, function () {
+            line.stop().animate({
+                width: default_width,
+                left: default_pos
+            }, 500);
+        });
+
+    };
+})(jQuery);
